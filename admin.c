48166ac287bfa3501161386ad419144639f3c9e5
Logan Gore <gorel@purdue.edu>
2013-08-01 20:46:50 -0400

    Added admin help text, and admins can no longer be muted/kicked

--- admin.c
+++ admin.c
@@ -11,8 +11,15 @@ bool handle_admin_action(struct user **users, fd_set *master, struct user *sende
 	//Extract the action with a string tokenizer
 	char *action = strtok(new_msg, " ");
 	
+	//If the sender is trying to find the admin function list, send it to them
+	if (!strncmp("!adminhelp", action, strlen("!adminhelp ")))
+	{
+		send_admin_help_text(sender);
+		return TRUE;
+	}
+	
 	//If the sender is trying to promote someone to an admin, find the target and call promote_to_admin
-	if (!strncmp("!admin", action, strlen("!admin ")))
+	if (!strncmp("!promote", action, strlen("!promote ")))
 	{
 		//Find the target user with the string tokenizer
 		char *name = strtok(NULL, " ");
@@ -77,7 +84,7 @@ void send_admin_help_text(struct user *user)
 		cJSON *sendJSON = cJSON_CreateObject();
 	
 		//The standard admin help text
-		static char *helptext = "\nType !admin <user> to give <user> admin privileges\nType !mute <user> to mute the given user\nType !unmute <user> to unmute the given user\nType !kick <user> to kick the given user\n";
+		static char *helptext = "\nType !promote <user> to give <user> admin privileges\nType !mute <user> to mute the given user\nType !unmute <user> to unmute the given user\nType !kick <user> to kick the given user\n";
 		
 		//Fill in the JSON data
 		cJSON_AddNumberToObject(sendJSON, "mlen", strlen(helptext));
@@ -169,11 +176,53 @@ void mute(struct user *admin, struct user *user_to_mute)
 	
 	//If "admin" has admin privileges, mute the given user
 	if (admin->admin)
+	{
+		//If the target is an admin, don't let them be muted
+		if (user_to_mute->admin)
+		{
+			send_user_is_admin_message(admin);
+			return;
+		}
+		
 		user_to_mute->muted = TRUE;
+		send_mute_message(user_to_mute, admin);
+	}
 	//Otherwise, tell the user that they don't have privileges for that
 	else
 		send_not_admin_message(admin);
 	
+}
+
+/* Send a message to the given user telling them they have been muted by the given admin */
+void send_mute_message(struct user *user, struct user *admin)
+{
+	//Create a cJSON object to hold the information
+    cJSON *sendJSON = cJSON_CreateObject();
+    
+    //Allocate space for the mute text
+    char mute_text[strlen(admin->name) + strlen(" has muted you.") + 1];
+    
+    //Print the string to the mute text
+    sprintf(mute_text, "%s has muted you.", admin->name);
+    
+    //Fill in the JSON data
+    cJSON_AddStringToObject(sendJSON, "from", "SERVER");
+    cJSON_AddNumberToObject(sendJSON, "mlen", strlen(mute_text));
+    cJSON_AddStringToObject(sendJSON, "msg", mute_text);
+    cJSON_AddNumberToObject(sendJSON, "private", TRUE);
+    
+    //Get the string representation of the JSON object
+    char *send_msg = cJSON_Print(sendJSON);
+    
+    //Print the message to the server's console
+    printf("%s has muted %s.\n", admin->name, user->name);
+    
+    //Send the message to the specified user
+   	send_to_user(send_msg, user);
+   	
+   	//Delete the cJSON object and free send_msg
+   	cJSON_Delete(sendJSON);
+   	free(send_msg);
 }
 
 /* Unmute the given user (will check that admin has the correct privileges) */
@@ -188,10 +237,45 @@ void unmute(struct user *admin, struct user *user_to_unmute)
 	
 	//If "admin" has admin privileges, unmute the given user
 	if (admin->admin)
+	{
 		user_to_unmute->muted = FALSE;
+		send_unmute_message(user_to_unmute, admin);
+	}
 	//Otherwise, tell the user that they don't have privileges for that
 	else
 		send_not_admin_message(admin);
+}
+
+/* Send a message to the given user telling them they have been unmuted by the given admin */
+void send_unmute_message(struct user *user, struct user *admin)
+{
+	//Create a cJSON object to hold the information
+    cJSON *sendJSON = cJSON_CreateObject();
+    
+    //Allocate space for the mute text
+    char unmute_text[strlen(admin->name) + strlen(" has unmuted you.") + 1];
+    
+    //Print the string to the mute text
+    sprintf(unmute_text, "%s has unmuted you.", admin->name);
+    
+    //Fill in the JSON data
+    cJSON_AddStringToObject(sendJSON, "from", "SERVER");
+    cJSON_AddNumberToObject(sendJSON, "mlen", strlen(unmute_text));
+    cJSON_AddStringToObject(sendJSON, "msg", unmute_text);
+    cJSON_AddNumberToObject(sendJSON, "private", TRUE);
+    
+    //Get the string representation of the JSON object
+    char *send_msg = cJSON_Print(sendJSON);
+    
+    //Print the message to the server's console
+    printf("%s has unmuted %s.\n", admin->name, user->name);
+    
+    //Send the message to the specified user
+   	send_to_user(send_msg, user);
+   	
+   	//Delete the cJSON object and free send_msg
+   	cJSON_Delete(sendJSON);
+   	free(send_msg);
 }
 
 /* Kick the given user out of the chat room for the given reason (will check that admin has the correct privileges) */
@@ -207,16 +291,23 @@ void kick(struct user **users, fd_set *master, struct user *admin, struct user *
 	//If "admin" has admin privileges, kick the given user
 	if (admin->admin)
 	{
+		//If the target is an admin, don't let them be muted
+		if (user_to_kick->admin)
+		{
+			send_user_is_admin_message(admin);
+			return;
+		}
+		
 		//Create a cJSON object to store the data
 		cJSON *sendJSON = cJSON_CreateObject();
 		
 		//Allocate space for the kick message ("<name> being kicked for <reason>")
-		char kick_msg[strlen(user_to_kick->name) + strlen(" being kicked for ") + strlen(reason) + 1];
+		char kick_msg[strlen(user_to_kick->name) + strlen(" has been kicked for ") + strlen(reason) + 1];
 
-		//Print "<name> being kicked for <reason>" to the kick_msg string
+		//Print "<name> has been kicked for <reason>" to the kick_msg string
 		sprintf(kick_msg, "%s being kicked for %s", user_to_kick->name, reason);
 
-		//Print to the server console that a user is being kicked
+		//Print to the server console that a user has been kicked
 		printf("%s\n", kick_msg);
 
 		//Add the data to the sendJSON
@@ -230,6 +321,9 @@ void kick(struct user **users, fd_set *master, struct user *admin, struct user *
 		
 		//Send a message to all users stating why user_to_kick is being removed
 		send_to_all(*users, send_msg, NULL);
+
+		//Tell everyone that the kicked user has left the chat		
+		send_user_left_message(*users, user_to_kick);
 		
 		//Remove the user from the linked list of users
         FD_CLR(user_to_kick->fd, master);
@@ -251,7 +345,33 @@ void send_not_admin_message(struct user *user)
 	cJSON *sendJSON = cJSON_CreateObject();
 	
 	//Define the error message ("Error: You must be an admin to do that.")
-	char *error_msg = "Error: You must be an admin to do that.";
+	char *error_msg = "You must be an admin to do that.";
+	
+	//Add the data to the sendJSON object
+	cJSON_AddStringToObject(sendJSON, "from", "SERVER");
+	cJSON_AddNumberToObject(sendJSON, "mlen", strlen(error_msg));
+	cJSON_AddStringToObject(sendJSON, "msg", error_msg);
+	cJSON_AddNumberToObject(sendJSON, "private", FALSE);
+	
+	//Print the JSON object to a string
+	char *send_msg = cJSON_Print(sendJSON);
+	
+	//Send the error message to the user trying to do whatever is requiring admin privileges
+	send_to_user(send_msg, user);
+	
+	//Free the cJSON object and send_msg
+	cJSON_Delete(sendJSON);
+	free(send_msg);
+}
+
+/* Tell the specified admin that they are trying to mute/kick an admin, which is not allowed */
+void send_user_is_admin_message(struct user *user)
+{
+	//Create a cJSON object to store the data
+	cJSON *sendJSON = cJSON_CreateObject();
+	
+	//Define the error message ("Error: You must be an admin to do that.")
+	char *error_msg = "Error: You cannot mute or kick someone with admin privileges.";
 	
 	//Add the data to the sendJSON object
 	cJSON_AddStringToObject(sendJSON, "from", "SERVER");
--- admin.h
+++ admin.h
@@ -20,13 +20,22 @@ void send_new_admin_message(struct user *users, char *new_admin_name);
 /* Mute the given user (will check that admin has the correct privileges) */
 void mute(struct user *admin, struct user *user_to_mute);
 
+/* Send a message to the given user telling them they have been muted by the given admin */
+void send_mute_message(struct user *user, struct user *admin);
+
 /* Unmute the given user (will check that admin has the correct privileges) */
 void unmute(struct user *admin, struct user *user_to_unmute);
+
+/* Send a message to the given user telling them they have been unmuted by the given admin */
+void send_unmute_message(struct user *user, struct user *admin);
 
 /* Kick the given user out of the chat room (will check that admin has the correct privileges) */
 void kick(struct user **users, fd_set *master, struct user *admin, struct user *user_to_kick, char *reason);
 
 /* Tell the specified user that they do not have admin privileges */
 void send_not_admin_message(struct user *user);
+
+/* Tell the specified admin that they are trying to mute/kick an admin, which is not allowed */
+void send_user_is_admin_message(struct user *user);
 
 #endif
